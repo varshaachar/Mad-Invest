4990eaa542a9a576432b6d08dbc288f9ab31dfc9
Zui <zuikng@gmail.com>
2017-12-02 05:53:46 -0500

    Logging sentiment

--- mad_invest/sentiment.py
+++ mad_invest/sentiment.py
@@ -1,9 +1,12 @@
+import logging
+
 import requests
 import time
 from mad_invest.config import db
 
 from mad_invest import config
 
+l = logging.getLogger(__name__)
 url = "https://language.googleapis.com/v1/documents:analyzeSentiment?key=" + config.GAPI
 
 
@@ -37,9 +40,11 @@ def get_sentimental():
 
     while True:
         time.sleep(2)
-        to_analyse = db["tweets"].find({"lang": "en", "score": {"$exists": False}})
+        to_analyse = db["tweets"].find({"lang": "en", "score": {"$exists": False}}).limit(100)
+
         for tweet in to_analyse:
             r = analyzeSentiment(tweet["text"])
+            l.debug("Sentiment for %s is %s", tweet["text"], r)
             score = r["documentSentiment"]["score"]
             magnitude = r["documentSentiment"]["magnitude"]
             db["tweets"].update_one({"_id": tweet["_id"]}, {
