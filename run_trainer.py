99399792c8ba95ff8288c65cc6310625cc879ead
Zui <zuikng@gmail.com>
2017-12-02 20:48:53 -0500

    More improvement
    - Use more than one csv.
    - Get label by month

--- run_trainer.py
+++ run_trainer.py
@@ -1,4 +1,9 @@
+import logging
+
 from mad_invest.trainner import main
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG,
+                        format="%(asctime)s [ %(threadName)s ] [ %(levelname)s ] : %(message)s'")
+
     main()
