3230f1633857b25a2169f7454cf9d7c4b9cba111
Zui <zuikng@gmail.com>
2017-12-03 06:54:54 -0500

    Invest?

--- chatbot2/app.js
+++ chatbot2/app.js
@@ -1,27 +1,45 @@
-const PAGE_ACCESS_TOKEN="EAAbSXaZCiipYBAPgmdZAvHEMYsC4IPm61KJNMuINWvaZCUJ8MVQV3QShyaooGLRWgmK2Rsi7tBnZBndr27vW21D8SoZC5fQHdFllXFd7QNDHt1SQIwYsiTZB4CKmT64BTU24wnjkgHlKfOlTRs6zpils0svdX2QNymaBDn12xZAWMobnNb9niTh"
+const PAGE_ACCESS_TOKEN = "EAAbSXaZCiipYBAPgmdZAvHEMYsC4IPm61KJNMuINWvaZCUJ8MVQV3QShyaooGLRWgmK2Rsi7tBnZBndr27vW21D8SoZC5fQHdFllXFd7QNDHt1SQIwYsiTZB4CKmT64BTU24wnjkgHlKfOlTRs6zpils0svdX2QNymaBDn12xZAWMobnNb9niTh"
 
-module.exports = handleMessage, handlePostback;
+const request = require('request');
+
+module.exports = handleMessage;
 
 // Handles messages events
 function handleMessage(sender_psid, received_message) {
-      let response;
-      // Check if the message contains text
+    let response;
+    // Check if the message contains text
     console.log(received_message);
-      if (received_message.text) {
-          if(received_message.text==="subscribe"){
-              response={"text": "Happy Mining"};
-          }else if (received_message.text==="unsubscribe"){
-              response = {"text": "No longer Mining? I hope you have a wonderful day with government controlled currency"};
-          }else if (received_message.text==="invest") {
-              
-          }else{
+    if (received_message.text) {
+        if (received_message.text === "subscribe") {
+            response = {"text": "Happy Mining"};
+        } else if (received_message.text === "unsubscribe") {
+            response = {"text": "No longer Mining? I hope you have a wonderful day with government controlled currency"};
+        } else if (received_message.text === "invest") {
+            request({
+                "uri": "http://35.196.69.223:27019/invest"
+            }, (err, res, body) => {
+                if (!err) {
+                    console.log('message sent!');
+                    let r = res.json();
+
+                    if (r["invest"] === "yes") {
+                        response = {"text": "You should invest!"}
+                    } else {
+                        response = {"text": "Nah man!"}
+                    }
+                    callSendAPI(sender_psid, response);
+                } else {
+                    console.error("Unable to send message:" + err);
+                }
+            });
+        } else {
             // Create the payload for a basic text message
             response = {"text": "HI!!!"};
         }
-      }
-      console.log(response);
-  // Sends the response message
-  callSendAPI(sender_psid, response);
+    }
+    console.log(response);
+    // Sends the response message
+    callSendAPI(sender_psid, response);
 }
 
 // // Handles messaging_postbacks events
@@ -43,27 +61,27 @@ function handleMessage(sender_psid, received_message) {
 
 // Sends response messages via the Send API
 function callSendAPI(sender_psid, response) {
-      // Construct the message body
-  let request_body = {
-    "recipient": {
-      "id": sender_psid
-    },
-    "message": response
-  }
-  console.log(request_body);
-  // Send the HTTP request to the Messenger Platform
-    var request = require('request');
+    // Construct the message body
+    let request_body = {
+        "recipient": {
+            "id": sender_psid
+        },
+        "message": response
+    };
+    console.log(request_body);
+    // Send the HTTP request to the Messenger Platform
+
     request({
-    "uri": "https://graph.facebook.com/v2.6/me/messages",
-    "qs": { "access_token": PAGE_ACCESS_TOKEN },
-    "method": "POST",
-    "json": request_body
-  }, (err, res, body) => {
-    if (!err) {
-      console.log('message sent!');
-      console.log(response);
-    } else {
-      console.error("Unable to send message:" + err);
-    }
-  });
+        "uri": "https://graph.facebook.com/v2.6/me/messages",
+        "qs": {"access_token": PAGE_ACCESS_TOKEN},
+        "method": "POST",
+        "json": request_body
+    }, (err, res, body) => {
+        if (!err) {
+            console.log('message sent!');
+            console.log(response);
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
 }
