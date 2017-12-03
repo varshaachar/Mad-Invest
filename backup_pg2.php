e3807f80780713a2a027ae3dcbea932de73d725d
kzhang21 <kellyzhang2197@gmail.com>
2017-12-03 00:25:58 -0500

    front end stuff
    please implement backup _pg2

---
+++ backup_pg2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width-device-width, initial-scale = 1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" href="hh_style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+    <link href='https://fonts.googleapis.com/css?family=Alegreya' rel='stylesheet'>
+    <script scr="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/jk/bootstrap.min.js"></script>
+    <script type='text/javascript' src='js/hh_java.js'></script>
+
+</head>
+<body>
+
+<!--start code here-->
+
+    
+<!--tweeninggg and other animations teehee-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.3/TweenMax.min.js"></script>
+<script type='text/javascript' src='js/jquery.particleground.js'></script>
+<script type='text/javascript' src='js/jquery-1.11.1.min.js'></script>
+
+
+</body>
+</html>
