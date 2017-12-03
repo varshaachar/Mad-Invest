e5057945eb8f2254e799f003b0317810c5a7a5cb
sarmsaad <sarah.msaad@yahoo.com>
2017-12-03 02:41:48 -0500

    make average by the hour

--- averaging.py
+++ averaging.py
@@ -1,8 +1,8 @@
-import pandas
 import csv
 from pymongo import MongoClient
 from sentiment import analyzeSentiment
 import logging
+from datetime import timedelta
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("averaging")
@@ -31,5 +31,35 @@ def csvCreate():
             except:
                 pass
 
+def categorize():
+    reader = csv.reader(open('tweets.csv', 'r'), delimiter=',')
+    row1 = next(reader)
+    row1 = next(reader)
+    with open('averages.csv','w') as file:
+        fields = ['date', 'score', 'magnitude']
+        writer = csv.DictWriter(file, fieldnames=fields)
+        writer.writeheader()
+        compare = (float(row1[3]) / 1000 )+ 3600
+        magn=[]
+        score=[]
+        for row in reader:
+            date = (float(row[3]) / 1000 )
+            if date <= compare:
+                score.append(float(row[1]))
+                magn.append(float(row[2]))
+            else:
+                #add final averages to csv
+                writer.writerow({'date': compare, 'score': sum(score) / float(len(score)), 'magnitude':sum(magn) / float(len(magn))})
+                magn = []
+                score = []
+                score.append(float(row[1]))
+                magn.append(float(row[2]))
+                compare = compare + timedelta(hours=1)
+        #once we exit the loop there might still be some data left
+        writer.writerow({'date': compare, 'score': sum(score) / float(len(score)), 'magnitude': sum(magn) / float(len(magn))})
+
+
+
+
 if __name__ == '__main__':
-    csvCreate()
+    categorize()
