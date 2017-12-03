a382d321c759769751d2089c45e44902d31ef301
kzhang21 <kellyzhang2197@gmail.com>
2017-12-03 03:12:22 -0500

    ignore this

--- frontEnd/backup_pg2.php
+++ frontEnd/backup_pg2.php
@@ -14,7 +14,10 @@
 <body>
 
 <!--start code here-->
-
+<?php
+$Intro = htmlspecialchars($_GET["bitcoin"]);
+echo "$Intro";
+?>
     
 <!--tweeninggg and other animations teehee-->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.20.3/TweenMax.min.js"></script>
